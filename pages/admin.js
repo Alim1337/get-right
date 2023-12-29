23dfd819c25dd33d6af409aefffa3cf524329624
Alim <118141673+Alim1337@users.noreply.github.com>
2023-12-29 15:41:31 +0100

    admin update

---
+++ pages/admin.js
@@ -0,0 +1,11 @@
+import Dashboard from '../components/dashboard/Dashboard';
+import Layout from '../components/layout/Layout';
+
+
+export default function Admin() {
+  return (
+    <Layout title='Home Layout'>
+        <Dashboard />
+    </Layout>
+  )
+}
