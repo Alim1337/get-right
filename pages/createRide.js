8aa81d37d63c9408836be87c1975e2fa73bdec96
RacimFethallah <RacimFethallah@proton.me>
2024-01-08 21:49:33 +0100

    Publish road not finished yet

--- pages/createRide.js
+++ pages/createRide.js
@@ -127,7 +127,7 @@ const CreateRide = () => {
             )}
           </div>
           <div className='mt-10 text-center'>
-            <button disabled={dateError || seatError} type="submit" className=" left-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            <button disabled={dateError || seatError || true} type="submit" className=" left-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
               Publish Ride
             </button>
           </div>
