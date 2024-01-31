8651ce10454e083029fafb1a16a6fc6b7a4c077b
RacimFethallah <RacimFethallah@proton.me>
2024-01-31 13:03:15 +0100

    Update user authentication logic Refactor database connection code Fix bug in data validation Add new feature for user profile picture upload Optimize database query performance

--- pages/proposeDrive.js
+++ pages/proposeDrive.js
@@ -42,11 +42,11 @@ const ProposeDrive = () => {
     const decodedToken = JSON.parse(atob(token.split('.')[1]));
     setUserId(decodedToken.userId);
     console.log('this is decodedToken', decodedToken);
-}, []);
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     console.log('this is driver id ', userId);
-}, [userId]); // This useEffect runs whenever driverId changes
+  }, [userId]); // This useEffect runs whenever driverId changes
 
 
   const setupMap = () => {
@@ -131,12 +131,12 @@ useEffect(() => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       // Reverse geocode to get coordinates for departure and destination
       const pickupCoordinates = await reverseGeocodeCoordinates(pickup.locationName);
       const dropoffCoordinates = await reverseGeocodeCoordinates(dropoff.locationName);
-  
+
       const response = await fetch('/api/apiProposeDrive', {
         method: 'POST',
         headers: {
@@ -155,7 +155,7 @@ useEffect(() => {
           destinationLongitude: dropoffCoordinates.longitude,
         }),
       });
-  
+
       if (response.ok) {
         console.log('Drive proposed successfully');
         // Show a notification before redirecting
@@ -172,7 +172,7 @@ useEffect(() => {
       console.error('Error during ride creation:', error);
     }
   };
-  
+
   // Function to get coordinates from location name using reverse geocoding
   const reverseGeocodeCoordinates = async (locationName) => {
     try {
@@ -187,8 +187,8 @@ useEffect(() => {
       return { latitude: 0, longitude: 0 }; // Return default coordinates on error
     }
   };
-  
-  
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -222,6 +222,8 @@ useEffect(() => {
 
   return (
     <Wrapper>
+
+
       <ButtonContainer>
         <Link href="/" passHref>
           <BackButton>
@@ -230,101 +232,108 @@ useEffect(() => {
         </Link>
       </ButtonContainer>
 
-      <MapContainer id="map" />
-
-      <ContentContainer>
-        <FormContainer>
-          <h1 className="text-4xl mb-8 text-center font-serif text-blue-990">Proposer un trajet</h1>
-          <form
-            onSubmit={handleSubmit}
-            className="border-2 border-blue-500 rounded-xl px-8 pt-6 pb-8 mb-4"
-          >
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="departure">
-                Pickup Location:
-                <input
-                  type="text"
-                  name="departure"
-                  value={pickup.locationName}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </label>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="destination">
-                Destination:
-                <input
-                  type="text"
-                  name="destination"
-                  value={dropoff.locationName}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </label>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
-                Date:
-                <input
-                  type="date"
-                  name="date"
-                  value={rideDetails.date}
-                  onChange={handleDateChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </label>
-              {dateError && (
-                <p className="text-red-500 text-s text-center">{dateError}</p>
-              )}
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="time">
-                Time:
-                <input
-                  type="time"
-                  name="time"
-                  value={rideDetails.time}
-                  onChange={handleTimeChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </label>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="seatsAvailable">
-                Seats Available:
-                <input
-                  type="number"
-                  name="seatsAvailable"
-                  value={rideDetails.seatsAvailable}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </label>
-              {seatError && (
-                <p className="text-red-500 text-s text-center">{seatError}</p>
-              )}
-            </div>
-            <div className='mt-10 text-center'>
-              <button
-                disabled={dateError || seatError}
-                type="submit"
-                className="left-1/2 bg-blue-500 hover:bg-blue-700
+      <SecondWrapper>
+        <ContentContainer>
+          <FormContainer>
+            <h1 className="text-4xl mb-8 text-center font-serif text-blue-990">Proposer un trajet</h1>
+            <form
+              onSubmit={handleSubmit}
+              className="border-2 border-blue-500 rounded-xl px-8 pt-6 pb-8 mb-4"
+            >
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="departure">
+                  Pickup Location:
+                  <input
+                    type="text"
+                    name="departure"
+                    value={pickup.locationName}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="destination">
+                  Destination:
+                  <input
+                    type="text"
+                    name="destination"
+                    value={dropoff.locationName}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                  Date:
+                  <input
+                    type="date"
+                    name="date"
+                    value={rideDetails.date}
+                    onChange={handleDateChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+                {dateError && (
+                  <p className="text-red-500 text-s text-center">{dateError}</p>
+                )}
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="time">
+                  Time:
+                  <input
+                    type="time"
+                    name="time"
+                    value={rideDetails.time}
+                    onChange={handleTimeChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="seatsAvailable">
+                  Seats Available:
+                  <input
+                    type="number"
+                    name="seatsAvailable"
+                    value={rideDetails.seatsAvailable}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+                {seatError && (
+                  <p className="text-red-500 text-s text-center">{seatError}</p>
+                )}
+              </div>
+              <div className='mt-10 text-center'>
+                <button
+                  disabled={dateError || seatError}
+                  type="submit"
+                  className="left-1/2 bg-blue-500 hover:bg-blue-700
                  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Proposer le trajet
-              </button>
-            </div>
-          </form>
-        </FormContainer>
+                >
+                  Proposer le trajet
+                </button>
+              </div>
+            </form>
+          </FormContainer>
+        </ContentContainer>
 
-  
-      </ContentContainer>
+        <MapContainer id="map" />
+
+      </SecondWrapper>
+
+
     </Wrapper>
   );
 }
 const Wrapper = tw.div`
-  p-4 bg-gray-200 h-screen
+  pt-1 bg-gray-200 h-screen 
+`;
+
+const SecondWrapper = tw.div`
+  flex h-screen mt-2
 `;
 
 const ButtonContainer = tw.div`
@@ -334,16 +343,17 @@ const ButtonContainer = tw.div`
 const BackButton = tw.button``;
 
 const MapContainer = tw.div`
-  flex-1 h-96
+  flex-1 w-full rounded-xl
 `;
 
 const ContentContainer = tw.div`
-  flex-1 flex flex-col
+   flex flex-row mr-2 h-full
 `;
 
 const FormContainer = tw.div`
-  max-w-2xl mb-8 p-8 bg-white shadow-2xl rounded-xl
+   max-w-2xl mb-1 p-8 bg-white shadow-2xl rounded-xl
 `;
+
 
 
 export default ProposeDrive;
