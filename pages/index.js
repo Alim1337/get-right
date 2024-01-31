f7556562473ad9a733b196f3daafb3693f190e24
RacimFethallah <RacimFethallah@proton.me>
2024-01-31 19:30:49 +0100

    Update Map and ReservedRidesModal styles

--- pages/index.js
+++ pages/index.js
@@ -154,109 +154,77 @@ const Index = () => {
 
   return (
     <Wrapper>
-      <Map location={location} />
 
-      <ActionItems>
-        <Header>
-          <Profile className="flex items-center space-x-4 border-4 border-blue-600 p-4 rounded-lg shadow-lg">
-            <div className="text-2xl font-bold text-blue-700">
-              User Connected: {user && `${user.firstName} ${user.lastName}`}
-            </div>
-            <UserImage
-              src={user && user.photoUrl}
-              alt="User Photo"
-              className="h-16 w-16 cursor-pointer rounded-full border-4 border-blue-800"
-            />
-          </Profile>
-
-
-
-          <DisconnectButton onClick={handleDisconnect}>Disconnect</DisconnectButton>
-        </Header>
-
-        <ActionButtons>
-          <Link href="/search" passHref>
-            <ActionButton className="flex flex-col items-center justify-center mt-8">
-              <ActionButtonImage>
-                <FaCar size={50} />
-              </ActionButtonImage>
-              Serch Ride
-            </ActionButton>
-          </Link>
-
-          <Link href="/createRide" passHref>
-            <ActionButton className="flex flex-col items-center justify-center mt-8">
-              <ActionButtonImage>
-                <FaPlusCircle size={50} />
-              </ActionButtonImage>
-              Create Trip
-            </ActionButton>
-          </Link>
-
-
-          <Link href="/see_trips" passHref>
-            <ActionButton className="flex flex-col items-center justify-center mt-8">
-
-              <ActionButtonImage>
-                <FaCalendarAlt size={50} />
-              </ActionButtonImage>
-              See Trips
-            </ActionButton>
-          </Link>
-        </ActionButtons>
-
-
-        <div className="flex flex-col items-center justify-center mt-8">
-          {user && user.role === 'driver' ? (
-            <Link href="/manageProposedDrives" passHref>
-              <div className="w-1/2 text-center justify-center">
-                <ActionButton className="">Trajets proposé</ActionButton>
-              </div>
+      <SectionAside>
+        <Title>Get Right</Title>
+        <ActionItems>
+          <ActionButtons>
+            <Link href="/search" passHref>
+              <ActionButton className="flex flex-col items-center justify-center">
+                <ActionButtonImage>
+                  <FaCar size={50} />
+                </ActionButtonImage>
+                Serch Ride
+              </ActionButton>
             </Link>
-          ) : (
-            <Link href="/proposeDrive" passHref>
-              <div className=" w-1/2 text-center justify-center">
-                <ActionButton className="w-full text-center">
-                  Proposer un trajet
-                </ActionButton>
-              </div>
+
+            <Link href="/createRide" passHref>
+              <ActionButton className="flex flex-col items-center justify-center">
+                <ActionButtonImage>
+                  <FaPlusCircle size={50} />
+                </ActionButtonImage>
+                Create Trip
+              </ActionButton>
             </Link>
-          )}
-        </div>
 
-
-
-        <div className="flex flex-col items-center justify-center mt-3">
-          {user && user.role === 'driver' ? (
-            <Link href="/manageDrives" passHref>
-              <div className="w-full">
-                <ActionButton className="">Manage My Drives</ActionButton>
-              </div>
+            <Link href="/see_trips" passHref>
+              <ActionButton className="flex flex-col items-center justify-center">
+                <ActionButtonImage>
+                  <FaCalendarAlt size={50} />
+                </ActionButtonImage>
+                See Trips
+              </ActionButton>
             </Link>
-          ) : (
-            <div className="w-full text-center justify-center">
-              {hasReservations && (
-                <div className="flex items-center">
-                  <FaBell className="-mr-5" size={30} />
-                  <div className="bg-red-500 text-white text-center  mb-6 h-6 w-6 font-bold rounded-full mr-1">
-                    {counter}
-                  </div>
+          </ActionButtons>
 
-                  <ActionButton
-                    className="flex items-center w-full justify-center"
-                    onClick={handleShowReservedRides}
-                  >
-
-                    <div className="text-center">My reserved rides</div>
-                  </ActionButton>
+          <div className="flex flex-col items-center mt-auto w-full">
+            {user && user.role === 'driver' ? (
+              <Link href="/manageProposedDrives" passHref>
+                <div className="text-center justify-center">
+                  <ActionButtonBottom className="">Trajets proposé</ActionButtonBottom>
                 </div>
-              )}
-            </div>
+              </Link>
+            ) : (
+              <Link href="/proposeDrive" passHref>
+                <div className=" text-center justify-center">
+                  <ActionButtonBottom className="w-full text-center">
+                    Proposer un trajet
+                  </ActionButtonBottom>
+                </div>
+              </Link>
+            )}
+          </div>
+        </ActionItems>
+      </SectionAside>
 
-          )}
-        </div>
 
-      </ActionItems>
+
+
+
+      <SectionMain>
+        <NavBar>
+          <UserProfileSection
+            user={user}
+            counter={counter}
+            handleShowReservedRides={handleShowReservedRides}
+            handleDisconnect={handleDisconnect}
+          />
+        </NavBar>
+
+        <Map location={location} />
+
+      </SectionMain>
+
       {showReservedRidesModal && (
         <ReservedRidesModal
           reservations={reservations}
@@ -264,48 +232,126 @@ const Index = () => {
         />
       )}
 
+
+
     </Wrapper>
   );
 };
 
-const DisconnectButton = tw.button`
-  inline-block rounded-full bg-black text-white px-6 pb-2 pt-2.5 text-xl font-medium uppercase leading-normal text-center transition-all duration-500 ease-in-out hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50 active:bg-gray-900
-`;
+const UserProfileSection = ({ user, counter, handleShowReservedRides, handleDisconnect }) => (
+  <div className="flex items-center space-x-4 justify-between w-full">
+    {user && user.role === 'driver' ? (
+      <Link href="/manageDrives" passHref>
+        <ActionButtonReservedDrives>Manage My Drives</ActionButtonReservedDrives>
+      </Link>
+    ) : (
+      <div className="flex items-center space-x-2">
+        {counter > 0 && (
+          <div className="flex items-center">
+            <div className="bg-red-500 text-white text-center mb-8 -ml-2 absolute h-6 w-6 font-bold rounded-full">
+              {counter}
+            </div>
+            <ActionButtonReservedDrives onClick={handleShowReservedRides}>
+              My reserved rides
+            </ActionButtonReservedDrives>
+          </div>
+        )}
+      </div>
+    )}
 
-const ActionButton = tw.button`
-  inline-block w-full rounded-full bg-gray-200 text-black px-6 pb-2 pt-2.5 text-2xl font-serif uppercase leading-normal text-center shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition-all duration-500 ease-in-out hover:bg-gray-300 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-gray-300 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-gray-200 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-gray-300 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-gray-300 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-gray-300 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]
-`;
+    <div className="flex space-x-2 items-center ">
+      <Profile>
+        <div className="text-2xl font-bold text-blue-700">
+          {user && `${user.firstName} ${user.lastName}`}
+        </div>
+        <UserImage
+          src={user && user.photoUrl}
+          alt="User Photo"
+          className="h-16 w-16 cursor-pointer rounded-full border-4 border-blue-800"
+        />
+      </Profile>
+
+      <DisconnectButton onClick={handleDisconnect}>Disconnect</DisconnectButton>
+
+    </div>
+
+  </div>
+);
+
+
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen transition-all duration-500 ease-in-out
+  flex flex-row bg-white h-screen transition-all duration-500 ease-in-out
 `;
 
-const ActionItems = tw.div`
-  bg-white flex-1 p-4 transition-all duration-500 ease-in-out
+
+const Title = tw.h1`
+  text-4xl text-center text-indigo-600 font-bold tracking-wide mt-6 mb-4
 `;
 
-const Header = tw.div`
-  flex justify-between items-center transition-all duration-500 ease-in-out
+
+const SectionMain = tw.div`
+  flex-1 flex flex-col
+`;
+
+const NavBar = tw.div`
+ pl-8 pr-4 py-4 bg-gray-100 shadow-lg text-white flex flex-row justify-stretch items-center
+`;
+
+const ActionButtonReservedDrives = tw.button`
+ bg-green-500 text-xl text-white px-2 py-1 rounded-xl hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50
+`;
+
+
+
+
+const DisconnectButton = tw.button`
+  inline-block rounded-2xl bg-black h-10 text-white px-2 py-1 text-xl font-medium uppercase leading-normal text-center transition-all duration-500 ease-in-out hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50 active:bg-gray-900 
 `;
 
 const Profile = tw.div`
-  flex flex-row items-center transition-all duration-500 ease-in-out
-`;
-
-const Name = tw.div`
-  mr-2 text-sm transition-all duration-500 ease-in-out
+  flex items-center space-x-4 
 `;
 
 const UserImage = tw.img`
   h-8 w-auto cursor-pointer rounded-full transition-all duration-500 ease-in-out
 `;
 
+
+
+const ActionButton = tw.button`
+  inline-block flex flex-col items-center w-full rounded-2xl bg-gray-200 text-black px-2 pb-2 pt-2.5 text-2xl font-serif uppercase leading-normal text-center shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition-all duration-500 ease-in-out hover:bg-gray-300 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-gray-300 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-gray-200 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-gray-300 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-gray-300 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-gray-300 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]
+`;
+
+const ActionItems = tw.div`
+  flex flex-col transition-all duration-500 ease-in-out bg-white rounded-lg shadow-lg p-8 mb-2 h-full
+`;
+
 const ActionButtons = tw.div`
-  flex justify-between mt-4 transition-all duration-500 ease-in-out
+  flex flex-col justify-between transition-all duration-500 ease-in-out gap-6 mt-4
+`;
+
+const ActionButtonBottom = tw.button`
+  inline-block flex flex-col items-center w-full rounded-2xl bg-gray-200 text-black px-6 pb-2 pt-2.5 text-2xl font-serif uppercase leading-normal text-center shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition-all duration-500 ease-in-out hover:bg-gray-300 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-gray-300 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-gray-200 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-gray-300 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-gray-300 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-gray-300 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] 
+`;
+
+const SectionAside = tw.div`
+  w-1/4 bg-gray-300 flex flex-col items-center shadow-lg
 `;
 
 const ActionButtonImage = tw.div`
-  h-3/5 mb-4 transition-all duration-500 ease-in-out
+  h-3/5 mb-2 transition-all duration-500 ease-in-out
 `;
+
+// const Header = tw.div`
+//   flex justify-between items-center transition-all duration-500 ease-in-out
+// `;
+
+
+
+// const Name = tw.div`
+//   mr-2 text-sm transition-all duration-500 ease-in-out
+// `;
+
 
 export default Index;
