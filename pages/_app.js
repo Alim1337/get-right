b496267f49d94d28999b3435af0e9f1c3485187b
RacimFethallah <RacimFethallah@proton.me>
2024-01-31 11:36:15 +0100

    Add sonner library for toast notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "../styles/globals.css"
 import "tailwindcss/tailwind.css"
 import "mapbox-gl/dist/mapbox-gl.css"
+import { Toaster, toast } from 'sonner'
+
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Toaster position="top-right" expand={true} richColors  />
+            <Component {...pageProps} />
+        </>)
 }
 
 export default MyApp
