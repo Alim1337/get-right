e4d49c720cd1f63ae3246c70fddd859c8c0de2cb
RacimFethallah <RacimFethallah@proton.me>
2024-02-04 13:39:13 +0100

    Modifier le titre de la page de création de trajet

--- pages/createRide.js
+++ pages/createRide.js
@@ -360,7 +360,7 @@ const CreateRide = () => {
 
         <ContentContainer>
           <FormContainer>
-            <h1 className="text-4xl mb-8 text-center font-serif text-blue-990">Create a Ride</h1>
+            <h1 className="text-4xl mb-8 text-center font-serif text-blue-990">Create a Trip</h1>
             <form
               onSubmit={handleSubmit}
               className="border-2 border-blue-500 rounded-xl px-8 pt-6 pb-8 mb-4"
