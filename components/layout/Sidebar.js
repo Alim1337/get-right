1a47a2c0980deb5fe3686ff69ac940fc174a163e
Alim <118141673+Alim1337@users.noreply.github.com>
2026-05-08 08:11:45 +0100

    Update Sidebar.js

--- components/layout/Sidebar.js
+++ components/layout/Sidebar.js
@@ -2,8 +2,7 @@ import React, { useRef } from 'react';
 import OutsideClick from '../../utils/outsideClick';
 import Nav from './sidebar/Nav';
 import SettingButton from './sidebar/SettingButton';
-import Logo from './sidebar/logo';
-
+import Logo from './Sidebar/Logo';
 const Sidebar = ({mobileNavsidebar}) => {
     const sidebarRef = useRef(null);
     const sidebarOutsideClick = OutsideClick(sidebarRef);
